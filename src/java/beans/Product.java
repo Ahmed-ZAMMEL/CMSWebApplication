9380ceb4c4d2a32a5cb0a6f1b1c1b0534e1eb787
Ahmed <ahmed.zammel.info@gmail.com>
2016-08-17 20:34:56 +0200

    Add new bean for the produts

---
+++ src/java/beans/Product.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author zammel
+ */
+public class Product {
+
+    private String designation;
+    private String description;
+    private double price;
+    private int quantity;
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
